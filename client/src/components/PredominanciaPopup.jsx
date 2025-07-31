c165a37f28f8fb7a3061f0146d1ae5630251aeaa
vinivascdev <joaoviniciuspereiraa@gmail.com>
2025-07-30 23:32:26 -0300

    Correção caminho CSS

--- client/src/components/PredominanciaPopup.jsx
+++ client/src/components/PredominanciaPopup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/PredominanciaPopup.css';
+import '../styles/PredominanciaPopUp.css';
 
 export default function PredominanciaPopup({ dados }) {
   if (!dados) return null;
